6e20664e40f598e46ab23378bf1724f83c23344b
Suneel Potugari <potugarisuneel330@gmail.com>
2025-10-01 23:53:00 +0530

    Mongo db first commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+from us_visa.logger import logging
+# logging.info("welcome to our logging")
+from us_visa.exception import USvisaException
+import sys
+ 
+try:
+    x = 1/0
+except Exception as e:
+    logging.error(USvisaException(e,sys))
+
+    
